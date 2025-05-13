2cd130a08a2d17b6bd2188c14e1929c1aab25f16
rasmi <rrelasmar@gmail.com>
2025-05-13 15:35:28 +0000

    Added license headers.

--- anthro_benchmark/analysis/__init__.py
+++ anthro_benchmark/analysis/__init__.py
@@ -1,4 +1,16 @@
-# anthro_benchmark/analysis/__init__.py
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from .analysis import (
     run_analysis,
@@ -6,7 +18,7 @@ from .analysis import (
     add_category_counts,
     calculate_summary_stats,
     plot_cue_percentages,
-    plot_category_radar
+    plot_category_radar,
 )
 
 __all__ = [
@@ -15,5 +27,5 @@ __all__ = [
     "add_category_counts",
     "calculate_summary_stats",
     "plot_cue_percentages",
-    "plot_category_radar"
+    "plot_category_radar",
 ]
